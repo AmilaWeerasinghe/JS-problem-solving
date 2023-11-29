2fbc3042df429c6a17e67e8d4687d4a027d85b9d
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2023-11-29 18:44:19 +0530

    arrow function scoping

---
+++ arrowFunction.js
@@ -0,0 +1,21 @@
+class Person {
+    constructor(name){
+        this.name =name;
+    }
+
+    printNameArrowFunction () {
+        setTimeout(()=>{
+            console.log('Arrow :'+ this.name); // object scope
+        },100)
+    }
+
+    printNameNormal() {
+        setTimeout(function(){
+            console.log('Normal :',this.name); // redefined to scope where this function is called
+        },100)
+    }
+}
+
+const person = new Person('Bob');
+person.printNameArrowFunction();
+person.printNameNormal();
