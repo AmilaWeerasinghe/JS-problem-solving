ecd708e610a06d87116b21091b7186835dc79922
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2024-01-02 14:51:47 +0530

    modularization exports and imports to utils

--- JS_for_react/assets/scripts/functions.js
+++ JS_for_react/assets/scripts/functions.js
@@ -1,5 +1,10 @@
-const combine = () => {
+import testFun, {API_KEY, API_URL as url} from "./utils.js"
+
+const combine = (a,b,c) => {
     return a *b /c;
 }
 
-combine();
+combine(1,2,4);
+
+testFun();
+console.log(API_KEY, url)
---
+++ JS_for_react/assets/scripts/utils.js
@@ -0,0 +1,8 @@
+export const API_KEY = "abc1235dgh";
+export const API_URL = "https://test.com/test";
+
+const testFunction = () => {
+    console.log('hello util test function');
+}
+
+export default testFunction;
