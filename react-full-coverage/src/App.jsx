4ed3e1836e042140334bc2f7a367ae9fb67c3f42
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2024-01-03 16:09:04 +0530

    components passing functions as props with args

--- react-full-coverage/src/App.jsx
+++ react-full-coverage/src/App.jsx
@@ -1,8 +1,14 @@
 import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header';
 import CoreConcepts from './components/CoreConcepts/CoreConcepts';
+import TabButtons from './components/TabButtons/TabButtons';
+import {useState} from 'react'
 
 function App() {
+  let tabContent = 'Please select a tab';
+  const handleSelect= (selectedButton) => {
+    console.log('Handle Select',selectedButton)
+  }
   return (
     <div>
       <Header/>
@@ -15,6 +21,16 @@ function App() {
           })}
         </ul>
         </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButtons onSelect={() => handleSelect('components')}>Components</TabButtons>
+            <TabButtons onSelect={() => handleSelect('jsx')}>JSX</TabButtons>
+            <TabButtons onSelect={() => handleSelect('props')}>Props</TabButtons>
+            <TabButtons onSelect={() => handleSelect('states')}>States</TabButtons>
+          </menu>
+        </section>
+        
         
 
       </main>
---
+++ react-full-coverage/src/components/TabButtons/TabButtons.jsx
@@ -0,0 +1,7 @@
+const TabButtons = ({children, onSelect})=> {
+    return(
+        <li><button onClick={onSelect}>{children}</button></li>
+    )
+}
+
+export default TabButtons;
