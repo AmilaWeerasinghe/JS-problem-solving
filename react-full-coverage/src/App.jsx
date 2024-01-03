7f025d4eab1cb3b0a66bb69f84e8323c6ad24ad2
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2024-01-04 00:13:06 +0530

    dynamic interactive styles and elements

--- react-full-coverage/src/App.jsx
+++ react-full-coverage/src/App.jsx
@@ -2,12 +2,16 @@ import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header';
 import CoreConcepts from './components/CoreConcepts/CoreConcepts';
 import TabButtons from './components/TabButtons/TabButtons';
-import {useState} from 'react'
+import {useState} from 'react';
+import { EXAMPLES } from './data';
 
 function App() {
-  let tabContent = 'Please select a tab';
+  const [tabContent, setTabContent] = useState('components');
+  const [selectedTopic, setSelectedTopic] = useState(false);
+
   const handleSelect= (selectedButton) => {
-    console.log('Handle Select',selectedButton)
+    setTabContent(selectedButton);
+    setSelectedTopic(true)
   }
   return (
     <div>
@@ -17,22 +21,31 @@ function App() {
         <h2>Core Concepts</h2>
         <ul>
           {CORE_CONCEPTS.map((item)=>{
-            return(<CoreConcepts {...item}/>)
+            return(<CoreConcepts key={item.title} {...item}/>)
           })}
         </ul>
         </section>
         <section id='examples'>
           <h2>Examples</h2>
           <menu>
-            <TabButtons onSelect={() => handleSelect('components')}>Components</TabButtons>
-            <TabButtons onSelect={() => handleSelect('jsx')}>JSX</TabButtons>
-            <TabButtons onSelect={() => handleSelect('props')}>Props</TabButtons>
-            <TabButtons onSelect={() => handleSelect('states')}>States</TabButtons>
+            <TabButtons isSelected={tabContent === 'components'} onSelect={() => handleSelect('components')}>Components</TabButtons>
+            <TabButtons isSelected={tabContent === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButtons>
+            <TabButtons isSelected={tabContent === 'props'} onSelect={() => handleSelect('props')}>Props</TabButtons>
+            <TabButtons isSelected={tabContent === 'state'} onSelect={() => handleSelect('state')}>States</TabButtons>
           </menu>
+          
+            {!selectedTopic && <p>Please select a topic.</p>}
+            {selectedTopic && <div id='tab-content'>
+               <h3>{EXAMPLES[tabContent].title}</h3>
+            <p>{EXAMPLES[tabContent].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[tabContent].code}
+              </code>
+            </pre>
+            </div>}
+         
         </section>
-        
-        
-
       </main>
     </div>
   );
--- react-full-coverage/src/components/TabButtons/TabButtons.jsx
+++ react-full-coverage/src/components/TabButtons/TabButtons.jsx
@@ -1,6 +1,6 @@
-const TabButtons = ({children, onSelect})=> {
+const TabButtons = ({children, onSelect, isSelected})=> {
     return(
-        <li><button onClick={onSelect}>{children}</button></li>
+        <li><button className={isSelected ? 'active' : undefined}onClick={onSelect}>{children}</button></li>
     )
 }
 
