7f2c9804d31d83574ca08126093032118674c8b6
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2024-01-09 20:16:55 +0530

    useRefs to set independent timers for each component

---
+++ react-refs-and-portals/src/components/TimerChallenge.jsx
@@ -0,0 +1,36 @@
+import { useState, useRef } from "react";
+
+
+const TimerChallenge = ({title, targetTime})=>{
+    const timer = useRef();
+    const [timerStarted, setTimerStarted] = useState(false);
+    const [timerExpired , setTimerExpired] = useState(false);
+
+    const handleTimerStart = () =>{
+        setTimerStarted(true);
+        timer.current = setTimeout(()=>{
+            //will be executed once the allocated time is finished
+            setTimerExpired(true);
+        },targetTime*1000)
+    }
+
+    const handleTimerStop = ()=>{
+        clearTimeout(timer.current);
+        setTimerStarted(false)
+    }
+    return(
+        <section className="challenge">
+            <h2>{title}</h2>
+            {timerExpired && <p>You lost!</p>}
+            <p className="challenge-time">{targetTime} second{targetTime > 1 ? 's' : ''}</p>
+            <p>
+                <button onClick={timerStarted ? handleTimerStop : handleTimerStart}>{timerStarted ? 'Stop' : 'Start'} Challenge</button>
+            </p>
+            <p className={timerStarted ?  'active' : undefined}>
+                {timerStarted? 'Timer is running' : 'Timer inactive'}
+            </p>
+        </section>
+    )
+}
+
+export default TimerChallenge;
