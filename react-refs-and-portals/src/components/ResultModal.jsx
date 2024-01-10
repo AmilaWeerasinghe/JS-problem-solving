2d9afbce1ae0b737461b3b60c5b2fece0ee29380
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2024-01-10 13:55:48 +0530

    add score and remaining time

--- react-refs-and-portals/src/components/ResultModal.jsx
+++ react-refs-and-portals/src/components/ResultModal.jsx
@@ -1,15 +1,23 @@
 import { forwardRef } from "react";
-const ResultModal = forwardRef(({result, targetTime},ref) => {
+const ResultModal = forwardRef(({targetTime, remainingTime, onReset},ref) => {
+
+    const userLost = remainingTime <= 0;
+
+    const formattedRemainingTime = (remainingTime/1000).toFixed(2);
+
+    const score = Math.round((remainingTime/ (targetTime *1000)) *100);
+
     return(
         <dialog ref={ref} className="result-modal">
-            <h2>You {result}</h2>
+            {userLost && <h2>You lost</h2>}
+            {!userLost && <h2>You scored {score}</h2>}
             <p>
                 The target time was <strong>{targetTime}</strong> seconds.
             </p>
             <p>
-                You stopped the timer with <strong>X</strong> seconds left
+                You stopped the timer with <strong>{formattedRemainingTime}</strong> seconds left
             </p>
-            <form method="dialog">
+            <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
         </dialog>
