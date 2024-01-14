4dd3bc362608bfc90c2b32987b8e3ccfd095162f
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2024-01-14 18:26:52 +0530

    initialise app and add redux store to handle header

--- redux-side-effects/src/components/Shop/ProductItem.js
+++ redux-side-effects/src/components/Shop/ProductItem.js
@@ -1,8 +1,14 @@
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
+import {useDispatch} from 'react-redux';
 
 const ProductItem = (props) => {
+  const dispatch = useDispatch();
   const { title, price, description } = props;
+
+  const handleAddToCart = () =>{
+
+  }
 
   return (
     <li className={classes.item}>
@@ -13,7 +19,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={handleAddToCart}>Add to Cart</button>
         </div>
       </Card>
     </li>
--- redux-side-effects/src/store/index.js
+++ redux-side-effects/src/store/index.js
@@ -1,8 +1,32 @@
 import {configureStore} from '@reduxjs/toolkit';
+import {createSlice} from '@reduxjs/toolkit';
+import uiSlice from './ui-slice';
+
+const initialState = {
+    title :'test item',
+    quantity: 3,
+    total: 4,
+    price: 20
+}
+
+
+
+createSlice({
+    name: 'cart',
+    initialState,
+    reducers:{
+        AddItemToCart(state,action) {
+            
+        }
+    }
+})
+
 
 
 const store = configureStore({
     reducer:{
-
+       ui: uiSlice.reducer
     }
 })
+
+export default store;
