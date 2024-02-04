dbcf7cc2b5abd9c8c86420f4456756b756a46702
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2024-02-04 22:16:32 +0530

    fetch data from localstorage

--- react-sideEffects/src/App.jsx
+++ react-sideEffects/src/App.jsx
@@ -7,12 +7,16 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import {sortPlacesByDistance} from './loc.js'
 
+const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+  const storedPlaces = storedIds.map((id) => 
+    AVAILABLE_PLACES.find((place) => place.id===id) 
+  )
+
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
   const [availablePlaces, setAvailablePlaces] = useState([]);
-
 
   useEffect(()=>{
     navigator.geolocation.getCurrentPosition((position) => {
@@ -58,6 +62,9 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    localStorage.setItem('selectedPlaces',JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current)));
   }
 
   return (
