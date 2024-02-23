1228ba2655abe551aa533c05a34523b89bc79ca1
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2024-02-24 00:14:26 +0530

    types basics

---
+++ typescript-react/basics.ts
@@ -0,0 +1,21 @@
+const num: number = 10;
+const nameFirst:string = 'test';
+const married:boolean = true;
+
+let hobbies: string[];
+
+hobbies=['singing', 'dancing'];
+
+let person:{
+    name:string,
+    age:number
+};
+
+person = {
+    name: 'amila',
+    age: 27
+}
+
+// person = {
+//     isMarried: true
+// }
